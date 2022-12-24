088c8c61afccc886e35fe37142963ab66358a56a
Anca <anca-ioana.oprea@s.unibuc.ro>
2022-12-24 15:51:11 +0200

    fix errors

--- main.cpp
+++ main.cpp
@@ -14,12 +14,12 @@
 
 
 void read_Author(std::ifstream &fauthor, std::vector<Author> &authors){
-    std::string name, gender, nationality;
 
     fauthor.open ("database/authors.in");
     if (!fauthor.is_open()) std::cerr << "Error in opening the file" << std::endl;
     else{
         while(fauthor.good()){
+            std::string name, gender, nationality;
             std::getline(fauthor, name, ',');
             std::getline(fauthor, gender, ',');
             std::getline(fauthor, nationality, '\n');
@@ -33,18 +33,18 @@ void read_Author(std::ifstream &fauthor, std::vector<Author> &authors){
         }
     }
 
-//    for(auto & i : author) std::cout  << i << "\n";
+//    for(auto const & i : author) std::cout  << i << "\n";
 }
 
-void read_Publisher(std::ifstream &fpublisher, std::vector<Publisher> &publishers, std::vector<Author> &authors_list){
-    std::string name, author_name, buffer;
-    int authors_count;
-    std::vector <std::string> author_names;
+void read_Publisher(std::ifstream &fpublisher, std::vector<Publisher> &publishers, std::vector<Author> const &authors_list){
 
     fpublisher.open ("database/publishers.in");
     if (!fpublisher.is_open()) std::cerr << "Error in opening the file" << std::endl;
     else{
         while(fpublisher.good()){
+            std::string name, author_name, buffer;
+            int authors_count;
+            std::vector <std::string> author_names;
             std::getline(fpublisher, name, ',');
 //        std::cout << "name: " << name << std::endl;
             std::getline(fpublisher, buffer, ',');
@@ -68,20 +68,20 @@ void read_Publisher(std::ifstream &fpublisher, std::vector<Publisher> &publisher
         }
     }
 
-    for(auto & i : publishers) std::cout  << i << "\n";
+    for(auto const & i : publishers) std::cout  << i << "\n";
 }
 
 void read_book(std::ifstream &fbook, std::vector<Book> &books, std::vector<Author> &authors_list, std::vector<Publisher> &publisher_list){
-    std::string title, author_name, publisher_name;
-    std::map<std::string, Genre> pairs = { {"Fiction" , Genre::Fiction} , {"Nonfiction" , Genre::Nonfiction}, {"Drama" , Genre::Drama}, {"Folktale" , Genre::Folktale}, {"Poetry" , Genre::Poetry} };
-    double price;
-    Genre genre;
-    int year;
 
     fbook.open ("database/books.in");
     if (!fbook.is_open()) std::cerr << "Error in opening the file" << std::endl;
     else{
         while(fbook.good()){
+            std::string title, author_name, publisher_name;
+            std::map<std::string, Genre> pairs = { {"Fiction" , Genre::Fiction} , {"Nonfiction" , Genre::Nonfiction}, {"Drama" , Genre::Drama}, {"Folktale" , Genre::Folktale}, {"Poetry" , Genre::Poetry} };
+            double price;
+            Genre genre;
+            int year;
             std::getline(fbook, title, ',');
             std::getline(fbook, author_name, ',');
             std::getline(fbook, publisher_name, ',');
@@ -119,7 +119,7 @@ void read_book(std::ifstream &fbook, std::vector<Book> &books, std::vector<Autho
         }
     }
 
-//    for(auto & i : books) std::cout  << i << "\n";
+//    for(auto const & i : books) std::cout  << i << "\n";
 }
 
 int main() {
@@ -165,7 +165,7 @@ int main() {
 
     std::vector<Book> books_ = carturesti.find_by_word("ion");
     std::cout << "Cartile care contin sirul de caractere introdus sunt: \n";
-    for(auto & i : books_) std::cout  << i << "\n";
+    for(auto const & i : books_) std::cout  << i << "\n";
 
     double total_sales = carturesti.get_total_sales();
     std::cout << total_sales << std::endl;
